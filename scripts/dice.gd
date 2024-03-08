62c44fa373c613baaf0cc78c0af17ed8c9155ea0
nataljr <juniortl2005@gmail.com>
2024-03-07 22:44:10 -0300

    feat: dicearolly

--- scripts/dice.gd
+++ scripts/dice.gd
@@ -1,18 +1,34 @@
 extends RigidBody3D
 
 @export
-var xAxisRange = Vector2(5,5)
+var xAxisRange = Vector2(-100,100)
 @export
-var yAxisRange = Vector2(5,5)
+var yAxisRange = Vector2(-100,100)
 @export
-var zAxisRange = Vector2(5,5)
+var zAxisRange = Vector2(-100,100)
 @export
-var diceThrowDirectionImpulseRange = Vector2(5,5)
+var diceThrowDirectionImpulseRange = Vector2(-100,100)
+@export
+var offSetRange = Vector2(-500, 500)
 
 func _ready():
 	randomize()
-	var diceRandomDirection = Vector3(randf_range(xAxisRange.x, xAxisRange.y), randf_range(yAxisRange.x, yAxisRange.y), randf_range(zAxisRange.x, zAxisRange.y))
-	var diceRotationalImpulse = Vector3(randf_range(diceThrowDirectionImpulseRange.x, diceThrowDirectionImpulseRange.y), randf_range(diceThrowDirectionImpulseRange.x, diceThrowDirectionImpulseRange.y),  0)
+	var diceRandomDirection = Vector3(
+		randf_range(xAxisRange.x, xAxisRange.y), 
+		randf_range(yAxisRange.x, yAxisRange.y),
+		randf_range(zAxisRange.x, zAxisRange.y)
+	)
+	var diceRotationalImpulse = Vector3(
+		randf_range(diceThrowDirectionImpulseRange.x, diceThrowDirectionImpulseRange.y),
+		randf_range(diceThrowDirectionImpulseRange.x, diceThrowDirectionImpulseRange.y),
+		randf_range(diceThrowDirectionImpulseRange.x, diceThrowDirectionImpulseRange.y),
+	)
+	var offSetDirection = Vector3(
+		randf_range(offSetRange.x, offSetRange.y),
+		randf_range(offSetRange.x, offSetRange.y),
+		randf_range(offSetRange.x, offSetRange.y),
+	)
 	
 	apply_central_impulse(diceRandomDirection)
-	apply_impulse(diceRotationalImpulse,diceRandomDirection)
+	apply_force(diceRotationalImpulse, offSetDirection)
+
